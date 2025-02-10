7493c42087c34a583231030a3af57f64e4342f66
igor <mouradeoliveiraigor10@gmail.com>
2025-02-10 20:26:19 -0300

    aula 10.02

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,56 @@
     
 </head>
 <body>
-    
+
+
+  <div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+
+            <div class="logo">
+
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
+
+            <ol>
+
+
+            </ol>
+
+        </nav>
+
+        <div class="title">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluçoes em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex voluptas fugiat. Praesentium, assumenda nemo?</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
+
+  </div>
+
+
 </body>
 </html>
